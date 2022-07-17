92825e404439507be06b85ea8fe7c0b39a0e8e2d
Edinson Parra Bahos <edinsonpb@gmail.com>
2022-07-17 11:22:26 -0500

    cambio de tipado dinamico a tipado estatico

---
+++ retotipado.py
@@ -0,0 +1,15 @@
+def number_primo(numberp: int) -> int:
+        number_primo = input("digita un numero")
+            for i in range(i, number_primo):
+                if number_primo % i == 0
+                return("no es numero primo")
+
+
+
+
+
+def run():
+    print(number_primo)
+
+if __name__ == '__main__':
+    run()
