a18802550f53e0e7621c1b580f6a6a4b833ac484
Edinson Parra Bahos <edinsonpb@gmail.com>
2022-07-17 20:13:51 -0500

    operaciones con fecha

--- decorators.py
+++ decorators.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
 def execution_time(func):
-    def wrapper():
+    def wrapper(*args, **kwargs):
         initial_time = datetime.now()
-        func()
+        func(*args, **kwargs)
         final_time = datetime.now()
         time_elapse = final_time - initial_time
-        print("pasaron" + time_elapse.total_seconds() + " segundos")
+        print("pasaron " + str(time_elapse.total_seconds()) + " segundos")
     return wrapper
 
 @execution_time
@@ -14,4 +14,15 @@ def random_func():
     for _ in range(1, 1000000):
         pass
 
+@execution_time
+def suma(a: int, b: int) -> int:
+    return a + b
+
+@execution_time
+def saludo(nombre="Cesar"):
+    print("Hola " + nombre)
+
+
 random_func()
+suma(5, 5)
+saludo("Facundo")
---
+++ timesfunction.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+hoy = datetime.now()
+print(hoy)
+
+proxima = hoy + 8
+print(proxima)
