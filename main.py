76369794cef9d5fecb42294ce8deef3516cd1735
Edinson Parra Bahos <edinsonpb@gmail.com>
2022-07-23 16:33:27 -0500

    actualizacion de tabla con funcion

--- main.py
+++ main.py
@@ -15,6 +15,15 @@ def create_client(client_name):
         print("client already is in the client's list")
 
 
+def update_client(client_name, updated_client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ",", updated_client_name + ",")
+    else:
+        print("client is not in clients list")
+
+
 def list_clients():
     global clients
     print(clients)
@@ -33,6 +42,8 @@ def _print_welcome():
     print("[D]elete client")
     print("[U]pdate client")
 
+def _get_client_name():
+    return input("what is the client name? ")
 
 if __name__ == '__main__':
     _print_welcome()
@@ -41,12 +52,15 @@ if __name__ == '__main__':
     Command = Command.upper()
 
     if Command == "C":
-        client_name = input("what is the client name? ")
+        client_name = _get_client_name()
         create_client(client_name)
         list_clients()
     elif Command == "D":
         pass
-    elif: Command == "U":
-        pass
+    elif Command == "U":
+        client_name = _get_client_name()
+        updated_client_name = input("what is the updated client name? ")
+        update_client(client_name, updated_client_name)
+        list_clients()
     else:
         print("Invalid command")
