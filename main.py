33ea3d5074e4adb4ff8cb478ceed12cb0fb6036b
Edinson Parra Bahos <edinsonpb@gmail.com>
2022-07-23 18:47:20 -0500

    agregando busqueda a la funcion

--- main.py
+++ main.py
@@ -24,9 +24,28 @@ def update_client(client_name, updated_client_name):
         print("client is not in clients list")
 
 
+def delete_client(client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ",", "")
+    else:
+        print("client is not in clients list")
+
+
 def list_clients():
     global clients
     print(clients)
+
+
+def search_client(client_name):
+    clients_list = clients.split(",")
+
+    for client in clients_list:
+        if client != client_name:
+            continue
+        else:
+            return True
 
 
 def _add_comma():
@@ -41,9 +60,12 @@ def _print_welcome():
     print("[C]reate client")
     print("[D]elete client")
     print("[U]pdate client")
+    print("[S]earch client")
+
 
 def _get_client_name():
     return input("what is the client name? ")
+
 
 if __name__ == '__main__':
     _print_welcome()
@@ -56,11 +78,21 @@ if __name__ == '__main__':
         create_client(client_name)
         list_clients()
     elif Command == "D":
-        pass
+        client_name = _get_client_name()
+        delete_client(client_name)
+        list_clients()
     elif Command == "U":
         client_name = _get_client_name()
         updated_client_name = input("what is the updated client name? ")
         update_client(client_name, updated_client_name)
         list_clients()
+    elif Command == "S":
+        client_name = _get_client_name()
+        found = search_client(client_name)
+
+        if found:
+            print("The client is in the client's list")
+        else:
+            print("the client: {} is not in our cient's list", format(client_name))
     else:
         print("Invalid command")
