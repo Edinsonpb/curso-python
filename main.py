1fcbdf4ef424690c243821273e6989e25520e4e2
Edinson Parra Bahos <edinsonpb@gmail.com>
2022-07-22 21:26:07 -0500

    uso de funciones

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+from http import client
+from setuptools import Command
+
+
 clients = "pablo,ricardo,"
 
 
 def create_client(client_name):
     global clients
-    clients += client_name
-    _add_comma()
+    
+    if client_name not in clients:
+        clients += client_name
+        _add_comma()
+    else:
+        print("client already is in the client's list")
 
 
 def list_clients():
@@ -17,9 +25,24 @@ def _add_comma():
     clients += ","
 
 
-if __name__ == '__main__':
-    list_clients()
+def _print_welcome():
+    print("Welcome to platzi ventas")
+    print("*" * 50)
+    print("what would you like to do today?")
+    print("[C]reate client")
+    print("[D]elete client")
 
-    create_client("david")
-    
-    list_clients()
+
+if __name__ == '__main__':
+    _print_welcome()
+
+    Command = input()
+
+    if Command == "C":
+        client_name = input("what is the client name? ")
+        create_client(client_name)
+        list_clients()
+    elif Command == "D":
+        pass
+    else:
+        print("Invalid command")
